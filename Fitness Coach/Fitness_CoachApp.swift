c6ffee6431fbae7b69fa50ac264b76046aa3fcfe
isaac.chua <isaac.chua@bytedance.com>
2026-06-25 13:25:21 +0800

    Initial Commit

---
+++ Fitness Coach/Fitness_CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_CoachApp.swift
+//  Fitness Coach
+//
+//  Created by ByteDance on 25/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
